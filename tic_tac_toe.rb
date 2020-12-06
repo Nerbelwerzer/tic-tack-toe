e543e4c6358808e69ba2b5f2ae175c630e5385f8
Paul <paulfirmin1@gmail.com>
2020-12-06 23:04:22 +0000

    Refactored classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,19 +1,12 @@
 # frozen_string_literal: true
 
-class Game
+class Board
   def initialize
     @rows = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
-    @players = []
     @taken_squares = []
   end
 
-  def start_game
-    display_board
-    get_names
-    turn_loop(@players)
-  end
-
-  def display_board
+  def display
     puts " #{@rows[0][0]} | #{@rows[0][1]} | #{@rows[0][2]} "
     puts '-----------'
     puts " #{@rows[1][0]} | #{@rows[1][1]} | #{@rows[1][2]} "
@@ -21,54 +14,26 @@ class Game
     puts " #{@rows[2][0]} | #{@rows[2][1]} | #{@rows[2][2]} "
   end
 
-  def get_names
-    puts 'Welcome to ' + 'Noughts & Crosses'.bold + '. Please enter your name:'
-    @players.push(Player.new(gets.chomp.capitalize.red, 'X'.red))
-    puts "Greetings, #{@players[0].name}. I see you brought a friend. I'll be needing their name, too:"
-    @players.push(Player.new(gets.chomp.capitalize.green, 'O'.green))
-    puts "\n\n#{@players[0].name} vs. #{@players[1].name}"
-  end
-
-  def turn_loop(players)
-    loop do
-      turn(players[0])
-      turn(players[1])
-    end
-  end
-
-  def turn(player)
-    display_board
-    puts "\n#{player.name}'s turn: "
-    square = gets.chomp
-
-    if validate_choice?(square)
-      print_choice(square, player)
-      @taken_squares.push(square)
-    else puts "\n\nInvalid choice. Try again.".red
-         turn(player)
-    end
-
-    check_result(player)
-  end
-
-  def validate_choice?(square)
-    return true if !@taken_squares.include?(square) && square.to_i < 10 && square.to_i.positive?
-  end
-
-  def print_choice(square, player)
+  def print_choice(square, token)
     @rows.each do |row|
-      row[row.index(square)] = player.token if row.include?(square)
+      row[row.index(square)] = token if row.include?(square)
     end
+    @taken_squares.push(square)
   end
 
-  def check_result(player)
-    if diagonal_victory || vertical_victory || horizontal_victory
-      player.win
-      end_game("#{player.name} wins!")
-    elsif @taken_squares.length == 9
-      end_game('Draw!')
-    end
+  def available?(square)
+    @taken_squares.none?(square)
   end
+
+  def game_win
+    diagonal_victory || vertical_victory || horizontal_victory
+  end
+
+  def game_draw
+    @taken_squares.length == 9
+  end
+
+  private
 
   def diagonal_victory
     left_diagonal = [@rows[0][0], @rows[1][1], @rows[2][2]]
@@ -84,27 +49,67 @@ class Game
   def vertical_victory
     @rows.transpose.any?(&:all_equal?)
   end
+end
+
+class Game
+  def initialize(board)
+    @players = []
+    @board = board
+  end
+
+  def start_game
+    @board.display
+    get_names
+    game_loop(@players)
+  end
+
+  def get_names
+    puts 'Welcome to ' + 'Noughts & Crosses'.bold + '. Please enter your name:'
+    @players.push(Player.new(gets.chomp.capitalize.red, 'X'.red))
+    puts "Greetings, #{@players[0].name}. I see you brought a friend. I'll be needing their name, too:"
+    @players.push(Player.new(gets.chomp.capitalize.green, 'O'.green))
+    puts "\n\n#{@players[0].name} vs. #{@players[1].name}"
+  end
+
+  def game_loop(players)
+    loop do
+      turn(players[0])
+      turn(players[1])
+    end
+  end
+
+  def turn(player)
+    @board.display
+    puts "\n#{player.name}'s turn: "
+    choice = gets.chomp
+
+    if validate_choice?(choice)
+      @board.print_choice(choice, player.token)
+    else puts "\n\nInvalid choice. Try again.".red
+         turn(player)
+    end
+
+    check_result(player)
+  end
+
+  def validate_choice?(choice)
+    @board.available?(choice) && choice.to_i < 10 && choice.to_i.positive?
+  end
+
+  def check_result(player)
+    if @board.game_win
+      player.win
+      end_game("#{player.name} wins!")
+    elsif @board.game_draw
+      end_game('Draw!')
+    end
+  end
 
   def end_game(message)
-    display_board
+    @board.display
     puts message
     show_score
     rematch
-  end
-
-  def win(player)
-    display_board
-    puts "\n#{player.name} wins!\n\n"
-    player.wins += 1
-    show_score
-    rematch(player)
-  end
-
-  def draw(players)
-    display_board
-    puts "\nDraw!\n\n"
-    show_score
-    rematch(players)
   end
 
   def rematch
@@ -112,7 +117,7 @@ class Game
 
     if gets.chomp.downcase == 'y'
       reset_game
-      turn_loop(@players.swap!(0, 1))
+      game_loop(@players.swap!(0, 1))
     else
       exit
     end
@@ -134,7 +139,7 @@ class Array
   end
 
   def all_equal?
-    uniq.length === 1
+    uniq.length == 1
   end
 end
 
@@ -166,5 +171,5 @@ class String
   end
 end
 
-new_game = Game.new
+new_game = Game.new(Board.new)
 new_game.start_game
