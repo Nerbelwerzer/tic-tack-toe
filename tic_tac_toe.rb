71fa5a0b5e57a96b20cd87e3d2c9e5854c7b954f
Paul <paulfirmin1@gmail.com>
2020-12-06 22:24:14 +0000

    refactor

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,16 @@
+# frozen_string_literal: true
 
 class Game
   def initialize
     @rows = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
     @players = []
     @taken_squares = []
+  end
+
+  def start_game
+    display_board
+    get_names
+    turn_loop(@players)
   end
 
   def display_board
@@ -14,21 +21,15 @@ class Game
     puts " #{@rows[2][0]} | #{@rows[2][1]} | #{@rows[2][2]} "
   end
 
-  def start_game
-    display_board
-    get_names
-    turn_loop(@players)
-  end
-
   def get_names
     puts 'Welcome to ' + 'Noughts & Crosses'.bold + '. Please enter your name:'
-    @players.push(Player.new(gets.chomp.capitalize.red, 'X'.red)) 
+    @players.push(Player.new(gets.chomp.capitalize.red, 'X'.red))
     puts "Greetings, #{@players[0].name}. I see you brought a friend. I'll be needing their name, too:"
     @players.push(Player.new(gets.chomp.capitalize.green, 'O'.green))
     puts "\n\n#{@players[0].name} vs. #{@players[1].name}"
   end
 
-  def turn_loop(players)   
+  def turn_loop(players)
     loop do
       turn(players[0])
       turn(players[1])
@@ -51,7 +52,7 @@ class Game
   end
 
   def validate_choice?(square)
-    return true if !@taken_squares.include?(square) && square.to_i < 10 && square.to_i > 0
+    return true if !@taken_squares.include?(square) && square.to_i < 10 && square.to_i.positive?
   end
 
   def print_choice(square, player)
@@ -61,13 +62,34 @@ class Game
   end
 
   def check_result(player)
+    if diagonal_victory || vertical_victory || horizontal_victory
+      player.win
+      end_game("#{player.name} wins!")
+    elsif @taken_squares.length == 9
+      end_game('Draw!')
+    end
+  end
+
+  def diagonal_victory
     left_diagonal = [@rows[0][0], @rows[1][1], @rows[2][2]]
     right_diagonal = [@rows[0][2], @rows[1][1], @rows[2][0]]
 
-    win(player) if left_diagonal.all?(player.token) || right_diagonal.all?(player.token) #Diagonal victory
-    @rows.each { |row| win(player) if row.all?(player.token) } #Horizontal victory
-    @rows.transpose.each { |row| win(player) if row.all?(player.token) } #Vertical victory
-    draw(player) if @taken_squares.length > 8
+    left_diagonal.all_equal? || right_diagonal.all_equal?
+  end
+
+  def horizontal_victory
+    @rows.any?(&:all_equal?)
+  end
+
+  def vertical_victory
+    @rows.transpose.any?(&:all_equal?)
+  end
+
+  def end_game(message)
+    display_board
+    puts message
+    show_score
+    rematch
   end
 
   def win(player)
@@ -75,41 +97,47 @@ class Game
     puts "\n#{player.name} wins!\n\n"
     player.wins += 1
     show_score
-    rematch(player)   
+    rematch(player)
   end
 
   def draw(players)
     display_board
-    puts "\nResult = draw!\n\n"
+    puts "\nDraw!\n\n"
     show_score
     rematch(players)
   end
 
-  def rematch(players)
-    puts "Rematch? Y/N"
+  def rematch
+    puts 'Rematch? Y/N'
 
-    if gets.chomp.downcase == 'y' 
-      @rows = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
-      @taken_squares = []
-      turn_loop(@players.swap!(0,1))
+    if gets.chomp.downcase == 'y'
+      reset_game
+      turn_loop(@players.swap!(0, 1))
     else
       exit
     end
+  end
 
+  def reset_game
+    @rows = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
+    @taken_squares = []
   end
 
   def show_score
     puts "#{@players[0].name}  #{@players[0].wins} | #{@players[1].wins}  #{@players[1].name}"
   end
-
 end
 
 class Array
-  def swap!(a,b)
+  def swap!(a, b)
     self[a], self[b] = self[b], self[a]
   end
+
+  def all_equal?
+    uniq.length === 1
+  end
 end
-      
+
 class Player
   attr_reader :name, :token
   attr_accessor :wins
@@ -118,12 +146,24 @@ class Player
     @token = token
     @wins = 0
   end
+
+  def win
+    @wins += 1
+  end
 end
 
 class String
-  def green;"\e[32m#{self}\e[0m" end
-  def red;  "\e[31m#{self}\e[0m" end
-  def bold; "\e[1m#{self}\e[22m" end
+  def green
+    "\e[32m#{self}\e[0m"
+  end
+
+  def red
+    "\e[31m#{self}\e[0m"
+  end
+
+  def bold
+    "\e[1m#{self}\e[22m"
+  end
 end
 
 new_game = Game.new
